b56a33bb51fe7219680d938673b33f943d614b99
vhutshilo <mavhandu.vb@gmail.com>
2023-08-29 14:25:39 +0200

    push

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -39,15 +39,15 @@ export default async function About() {
                     href={`${data.resumeURL}?dl=${data.fullName}_resume`}
                     className="flex items-center justify-center gap-x-2 bg-[#1d1d20] border border-transparent hover:border-zinc-700 rounded-md duration-200 py-2 text-center cursor-cell font-medium"
                   >
-                    <BiFile className="text-base" /> Download Resumé
+                    <BiFile className="text-base" /> Download Resume
                   </a>
                 </div>
-                
+
                 <ul>
                   <li>
                     <a
                       href={`mailto:${data.email}`}
-                      className="flex items-center gap-x-2 hover:text-purple-400 duration-300"
+                      className="flex justify-center items-center gap-x-2 hover:text-purple-400 duration-300"
                     >
                       <BiEnvelope className="text-lg" />
                       {data.email}
--- app/(site)/components/global/Navbar.tsx
+++ app/(site)/components/global/Navbar.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "../../icons/logo.jpg";
+import Logo from "../../icons/logo.png";
 
 export default function Navbar() {
   return (
     <header className="py-6 md:px-16 px-6 border-b border-zinc-800 z-30 md:mb-28 mb-20">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
         <Link href="/">
-          <Image src={Logo} width={25} height={25} alt="logo" />
+          <Image src={Logo} width={80} height={80} alt="logo" />
         </Link>
         <nav>
           <ul className="flex items-center gap-x-8">
